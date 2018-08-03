590c8e3faee98f1670554ef6a0ef4a7bb1a2a11a
emkg <emkg@MacBook-Pro-4.local>
2018-08-03 18:00:26 -0500

    still thinking through data storage

--- src/components/Megastore.js
+++ src/components/Megastore.js
@@ -11,12 +11,7 @@ export default class Megastore extends React.Component {
   state = {};
 
   saveData = (data) => {
-    const newData = {...this.state, data}
-    this.setState({ newData })
-
-    console.log("new data: " + newData);
-    console.log("new state: " + this.state );
-
+    this.setState( data )
   }
 
   render() {
